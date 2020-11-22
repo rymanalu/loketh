8a8fb261c5b3ba1b5bc6f4c3c1b2851c488069af
Roni Yusuf Manalu <rymanalu@gmail.com>
2020-11-22 22:30:03 +0700

    Update footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,18 +7,40 @@ class Footer extends Component {
         <hr />
         <p className="text-center">
           Created by {
-            <a
-              href="https://rymanalu.github.io/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <ATargetBlank href="https://rymanalu.github.io/">
               Roni Yusuf
-            </a>
+            </ATargetBlank>
+          } &middot; {
+            <ATargetBlank href="https://www.linkedin.com/in/roni-yusuf/">
+              LinkedIn
+            </ATargetBlank>
+          } &middot; {
+            <ATargetBlank href="https://github.com/rymanalu">
+              Github
+            </ATargetBlank>
+          } &middot; {
+            <ATargetBlank href="https://github.com/rymanalu/loketh">
+              Source Code
+            </ATargetBlank>
           }
         </p>
       </footer>
     );
   }
 }
+
+const ATargetBlank = props => {
+  const { children, ...newProps } = props;
+
+  return (
+    <a
+      {...newProps}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {children}
+    </a>
+  );
+};
 
 export default Footer;
