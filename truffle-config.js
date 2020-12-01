78e022241d61b483d3c56e75210a68c3b595d298
Roni Yusuf Manalu <rymanalu@gmail.com>
2020-12-01 22:59:19 +0700

    Added env file & update config file for deploying to real network

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,30 @@
 const path = require('path');
 
+require('dotenv').config({ path: path.join(__dirname, '.env') });
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const {
+  NETWORK_ID,
+  NETWORK_ENDPOINT,
+  WALLET_MNEMONIC,
+  WALLET_ADDRESS_INDEX
+} = process.env;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
     develop: {
       port: 8545
+    },
+    production: {
+      provider: () => {
+        return new HDWalletProvider(
+          WALLET_MNEMONIC, NETWORK_ENDPOINT, WALLET_ADDRESS_INDEX
+        );
+      },
+      network_id: NETWORK_ID
     }
   },
   contracts_build_directory: path.join(__dirname, 'client', 'src', 'contracts'),
