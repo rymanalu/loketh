8b59bf00266a774c1a8b5389f774ef0d8516c399
Roni Yusuf Manalu <rymanalu@gmail.com>
2020-12-05 11:11:18 +0700

    Remove base url in router

--- client/src/App.js
+++ client/src/App.js
@@ -56,7 +56,7 @@ class App extends Component {
     const { accounts, initialized, loketh, web3 } = this.state;
 
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Navbar
           accounts={accounts}
           initialized={initialized}
